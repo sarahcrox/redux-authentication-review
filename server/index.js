d8c576c37a8133b35f8b6b05898d5df9aa7f769e
mattcbodily <mattcbodily@gmail.com>
2020-03-30 09:18:47 -0600

    added express-session

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express'),
+      session = require('express-session'),
       massive = require('massive'),
       authCtrl = require('./controllers/authCtrl'),
       ctrl = require('./controllers/mainCtrl'),
@@ -9,5 +10,12 @@ const express = require('express'),
 const app = express();
 
 app.use(express.json());
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    //add secret here
+    cookie: {maxAge: 1000 * 60 * 60 * 24}
+}))
 
 app.listen(3333, () => console.log(`Server is listening on 3333`))
